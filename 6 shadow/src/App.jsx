24ef5a28e940c53a405815376c8b387d0a373668
SUMIT SHARMA <sumit140803@gmail.com>
2025-08-02 01:03:33 +0530

    shadow

---
+++ 6 shadow/src/App.jsx
@@ -0,0 +1,55 @@
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+
+function App() {
+  return (
+    <>
+      <Canvas camera={{ position: [0, 3, 3] }} shadows>
+        <OrbitControls />
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[5, 5, 5]} intensity={0.5} castShadow 
+        shadow-camera-far={50}
+        shadow-camera-left={10}
+        shadow-camera-right={-10}
+        shadow-camera-top={10}
+        shadow-camera-bottom={-10}
+        />
+        <directionalLight position={[-5, 5, 5]} intensity={0.5} color="red" castShadow />
+
+        <mesh rotation-y={Math.PI / 4} castShadow receiveShadow>
+          <boxGeometry />
+          <meshStandardMaterial color="white" />
+        </mesh>
+
+        <mesh position={[1,1,1]} castShadow>
+          <sphereGeometry args={[0.5,32,32]} />
+          <meshStandardMaterial color={"white"} />
+        </mesh>
+
+        <mesh rotation-x={-Math.PI / 2} position-y={-0.5} receiveShadow>
+          <planeGeometry args={[5, 5]} />
+          <meshStandardMaterial color="white" />
+        </mesh>
+
+        <group position-x={[10]}>
+          <mesh rotation-y={Math.PI / 4} castShadow receiveShadow>
+          <boxGeometry />
+          <meshStandardMaterial color="white" />
+        </mesh>
+
+        <mesh position={[1,1,1]} castShadow>
+          <sphereGeometry args={[0.5,32,32]} />
+          <meshStandardMaterial color={"white"} />
+        </mesh>
+
+        <mesh rotation-x={-Math.PI / 2} position-y={-0.5} receiveShadow>
+          <planeGeometry args={[5, 5]} />
+          <meshStandardMaterial color="white" />
+        </mesh>
+        </group>
+      </Canvas>
+    </>
+  );
+}
+
+export default App;
